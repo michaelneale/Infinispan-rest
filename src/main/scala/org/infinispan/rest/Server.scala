d734b39aebd21b9ff4fb881910a55ef3895a1090
Michael Neale <michael.neale@gmail.com>
2009-08-23 14:09:46 +1000

    started infinispan integration

--- src/main/scala/org/infinispan/rest/Server.scala
+++ src/main/scala/org/infinispan/rest/Server.scala
@@ -1,18 +1,33 @@
 package org.infinispan.rest
 
+import java.io.InputStream
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.core.Response
+import manager.DefaultCacheManager
 
 
 
-@Path("/echo")
+@Path("/rest")
 class Server {
   @GET
-  @Path("/{message}")
-  def echoService(@PathParam("message") message: String) = {
-
-      Response.status(200).entity("here we are !").build();
+  @Path("/echo/{part1}/{message}")
+  def echoService(@PathParam("part1") p1: String, @PathParam("message") message: String) = {
+      println(p1 + " " + message)
+      Response.status(200).entity(message).build();
   }
+
+
+  @GET
+  @Path("/{cacheName}/{cacheKey}")
+  def getItem(@PathParam("cacheName") cacheName: String, @PathParam("cacheKey") keyName: String) = {
+      val ins: InputStream = null
+      new DefaultCacheManager(ins).getCache()
+  }
+
+
+
+  
+
 }
