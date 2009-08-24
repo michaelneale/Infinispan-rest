63cd08095d77934cc922d85394893e59af066a36
Michael Neale <michael.neale@gmail.com>
2009-08-24 18:41:25 +1000

    OK got lots more working...

--- src/main/scala/org/infinispan/rest/Server.scala
+++ src/main/scala/org/infinispan/rest/Server.scala
@@ -1,14 +1,12 @@
 package org.infinispan.rest
 
-import java.io.InputStream
+import java.io.{Serializable, InputStream}
+import javax.servlet.{ServletContextEvent, ServletContextListener}
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.core.Response
-import manager.DefaultCacheManager
-
-
-
+import manager.{CacheManager, DefaultCacheManager}
 @Path("/rest")
 class Server {
   @GET
@@ -31,3 +29,30 @@ class Server {
   
 
 }
+
+
+/**
+ * To init the cache manager. Nice to do this on startup as any config problems will be picked up before any
+ * requests are attempted to be serviced. Less kitten carnage.
+ */
+class AgentListener extends ServletContextListener {
+  def contextInitialized(ev: ServletContextEvent) = {
+    ManagerInstance.instance = new DefaultCacheManager("cache-config.xml")
+    ManagerInstance.instance.start
+  }
+  def contextDestroyed(ev: ServletContextEvent) = {
+    ManagerInstance.instance.stop
+  }
+}
+
+/**
+ * Just wrap a single instance of the Infinispan cache manager. 
+ */
+object ManagerInstance {
+   var instance: CacheManager = null
+   def getCache(name: String) = {
+      instance.getCache(name).asInstanceOf[Cache[String, Any]]
+   }
+}
+
+class CachedBlob(mimeType: String, data: Array[Byte]) extends Serializable
---
+++ src/test/scala/org/infinispan/rest/CacheTest.scala
@@ -0,0 +1,48 @@
+package org.infinispan.rest
+
+
+import config.{GlobalConfiguration, Configuration}
+import java.io.FileInputStream
+import junit.framework.TestCase
+import junit.framework.Assert._
+import transaction.lookup.DummyTransactionManagerLookup
+import manager.{CacheManager, DefaultCacheManager}
+/**
+ * 
+ * @author Michael Neale
+ */
+
+class CacheTest extends TestCase {
+  def testMichaelsIgnorance = {
+    //mostly for exploratory purposes....
+    val cm = new DefaultCacheManager("cache-config.xml")
+    val cache: Cache[String, Any] = cm.getCache.asInstanceOf[Cache[String, Any]]
+    cache.start
+    cache.put("Hey", "ho")
+    assertEquals("ho", cache.get("Hey"))
+    val cache1: Cache[String, Any] = cm.getCache("man").asInstanceOf[Cache[String, Any]]
+    cache1.put("Hey", "no")
+    cache1.put("Lets", "go")
+    assertEquals("no", cache1.get("Hey"))
+    assertEquals("ho", cache.get("Hey"))
+
+    assertFalse(cache.containsKey("Lets"))
+    assertTrue(cache1.containsKey("Lets"))
+
+
+    val cache11 = cm.getCache("man")
+    assertEquals("no", cache11.get("Hey"))
+
+
+
+
+  }
+
+  def testAnother = {
+    val cache: Cache[String, Any] = (new DefaultCacheManager(GlobalConfiguration.getNonClusteredDefault).getCache).asInstanceOf[Cache[String, Any]]
+    cache.start
+    cache.put("Hey", "ho")
+    assertEquals("ho", cache.get("Hey"))
+
+  }
+}
